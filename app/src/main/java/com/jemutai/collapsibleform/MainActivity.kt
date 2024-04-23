a19013b4635dc5cac0af71c077e126588311cddd
Jemutai <janetjemutai19@gmail.com>
2024-04-23 16:27:38 +0300

    initial commit

---
+++ app/src/main/java/com/jemutai/collapsibleform/MainActivity.kt
@@ -0,0 +1,188 @@
+package com.jemutai.collapsibleform
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.KeyboardArrowDown
+import androidx.compose.material.icons.rounded.KeyboardArrowUp
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.jemutai.collapsibleform.ui.theme.CollapsibleFormTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CollapsibleFormTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    PersonalDetailsSection()
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun PersonalDetailsSection() {
+    var isVisiblePersonalDetails by remember { mutableStateOf(false) }
+    var isVisiblePersonalAddress by remember { mutableStateOf(false) }
+
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clip(shape = RoundedCornerShape(10.dp))
+            .background(color = Color.White)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)
+        ) {
+            // Personal Details Section
+            PersonalDetailsCollapsibleSection(isVisible = isVisiblePersonalDetails) {
+                isVisiblePersonalDetails = it
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            // Personal Address Section
+            PersonalAddressCollapsibleSection(
+                isVisible = isVisiblePersonalAddress,
+                modifier = Modifier.padding(top = 16.dp)
+            ) {
+                isVisiblePersonalAddress = it
+            }
+        }
+    }
+}
+
+@Composable
+fun PersonalDetailsCollapsibleSection(
+    isVisible: Boolean,
+    onToggleVisibility: (Boolean) -> Unit
+) {
+    var iconState by remember { mutableStateOf(Icons.Rounded.KeyboardArrowDown) }
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(50.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .weight(1f)
+                .clickable {
+                    onToggleVisibility(!isVisible)
+                    iconState = if (isVisible) {
+                        Icons.Rounded.KeyboardArrowUp
+                    } else {
+                        Icons.Rounded.KeyboardArrowDown
+                    }
+                }
+                .background(Color.Red),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = "Beneficiary Personal Details",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                color = MaterialTheme.colorScheme.primary
+            )
+
+            Icon(
+                imageVector = iconState,
+                contentDescription = "Collapse/Expand",
+                tint = MaterialTheme.colorScheme.primary,
+                modifier = Modifier.align(Alignment.CenterEnd)
+            )
+        }
+    }
+
+    if (isVisible) {
+        PersonalDetailsForm()
+    }
+}
+
+@Composable
+fun PersonalAddressCollapsibleSection(
+    isVisible: Boolean,
+    modifier: Modifier = Modifier,
+    onToggleVisibility: (Boolean) -> Unit
+) {
+    var iconState by remember { mutableStateOf(Icons.Rounded.KeyboardArrowDown) }
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+            .fillMaxWidth()
+            .height(50.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .weight(1f)
+                .clickable {
+                    onToggleVisibility(!isVisible)
+                    iconState = if (isVisible) {
+                        Icons.Rounded.KeyboardArrowUp
+                    } else {
+                        Icons.Rounded.KeyboardArrowDown
+                    }
+                }
+                .background(Color.Green),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = "Personal Address",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                color = MaterialTheme.colorScheme.primary
+            )
+
+            Icon(
+                imageVector = iconState,
+                contentDescription = "Collapse/Expand",
+                tint = MaterialTheme.colorScheme.primary,
+                modifier = Modifier.align(Alignment.CenterEnd)
+            )
+        }
+    }
+
+    if (isVisible) {
+        PersonalAddressForm()
+    }
+}
+
---
+++ app/src/main/java/com/jemutai/collapsibleform/PersonalAddressForm.kt
@@ -0,0 +1,112 @@
+package com.jemutai.collapsibleform
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun PersonalAddressForm() {
+
+    var firstname by remember { mutableStateOf("") }
+    var middlename by remember { mutableStateOf("") }
+    var surname by remember { mutableStateOf("") }
+    var nationalid by remember { mutableStateOf("") }
+    var gender by remember { mutableStateOf("") }
+    var phonenumber by remember { mutableStateOf("") }
+    var nationality by remember { mutableStateOf("") }
+
+
+
+    Column(
+        modifier = Modifier
+            .fillMaxHeight(0.5f)
+            .padding(16.dp)
+            .verticalScroll(rememberScrollState()),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        OutlinedTextField(
+            value = firstname,
+            onValueChange = { firstname = it },
+            label = { Text("First Name") },
+            modifier = Modifier.padding(bottom = 16.dp),
+            shape = RoundedCornerShape(20.dp)
+        )
+        OutlinedTextField(
+            value = middlename,
+            onValueChange = { middlename = it },
+            label = { Text("Middle Name") },
+            modifier = Modifier.padding(bottom = 16.dp),
+            shape = RoundedCornerShape(20.dp)
+
+        )
+        OutlinedTextField(
+            value = surname,
+            onValueChange = { surname = it },
+            label = { Text("Sir Name") },
+            modifier = Modifier.padding(bottom = 16.dp),
+            shape = RoundedCornerShape(20.dp)
+
+        )
+        OutlinedTextField(
+            value = nationalid,
+            onValueChange = { nationalid = it },
+            label = { Text("National Identification Number") },
+            modifier = Modifier.padding(bottom = 16.dp),
+            shape = RoundedCornerShape(20.dp)
+
+        )
+        OutlinedTextField(
+            value = gender,
+            onValueChange = { gender = it },
+            label = { Text("Gender") },
+            modifier = Modifier.padding(bottom = 16.dp),
+            shape = RoundedCornerShape(20.dp)
+
+        )
+        OutlinedTextField(
+            value = phonenumber,
+            onValueChange = { phonenumber = it },
+            label = { Text("Phone Number") },
+            modifier = Modifier.padding(bottom = 16.dp),
+            shape = RoundedCornerShape(20.dp)
+
+        )
+        OutlinedTextField(
+            value = nationality,
+            onValueChange = { nationality = it },
+            label = { Text("Nationality") },
+            modifier = Modifier.padding(bottom = 16.dp),
+            shape = RoundedCornerShape(20.dp)
+
+        )
+
+
+        Button(
+            onClick = { /*TODO*/ },
+            shape = RoundedCornerShape(16.dp),
+            // colors = ButtonColors = buttonColors(Color.Red)
+
+        ) {
+            Text(text = "Save/Next")
+        }
+
+    }
+
+}
